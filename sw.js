e42edecf116153f887358dc4963406f2d50041ee
poritoca <poritoca@gmail.com>
2026-09-19 08:19:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 // Cache only this app's shell, and keep caches for other GitHub Pages projects intact.
 const PREFIX='ekikan-shell-'+encodeURIComponent(self.registration.scope)+'-';
-const CACHE=PREFIX+'17.0';
+const CACHE=PREFIX+'18.1';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./station_coverage_report.txt','./station_coverage_report.tsv','./station_support.json','./all_station_support.txt','./supported_stations.txt','./unsupported_stations.txt','./partially_supported_stations.txt','./source_inventory.tsv','./changes.txt','./validation_report.txt','./github_pages_fix.txt','./parser_regression_tests.txt'];
 const urls=SHELL.map(p=>new URL(p,self.registration.scope).href);
 self.addEventListener('install',event=>event.waitUntil((async()=>{const cache=await caches.open(CACHE);await cache.addAll(urls);await self.skipWaiting()})()));
