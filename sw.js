cd377f6bbea9ea12e4e961deabfe5870907ddf83
poritoca <poritoca@gmail.com>
2026-09-19 04:00:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 'use strict';
 // Cache only this app's shell, and keep caches for other GitHub Pages projects intact.
 const PREFIX='ekikan-shell-'+encodeURIComponent(self.registration.scope)+'-';
-const CACHE=PREFIX+'10';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./station_coverage_report_10.txt'];
+const CACHE=PREFIX+'13';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./station_coverage_report_13.txt','./station_coverage_report_13.tsv','./source_inventory_13.tsv','./changes_13.txt','./validation_report_13.txt'];
 const urls=SHELL.map(p=>new URL(p,self.registration.scope).href);
 self.addEventListener('install',event=>event.waitUntil((async()=>{const cache=await caches.open(CACHE);await cache.addAll(urls);await self.skipWaiting()})()));
 self.addEventListener('activate',event=>event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})()));
